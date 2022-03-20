5f3e494717094715eeeb834f12134f678f65ced7
RushiCodez <94935989+RushiCodez@users.noreply.github.com>
2022-03-20 18:19:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width: 350px;
+    height:300px;
+    image_format:'png',
+    png_quality:90;
+    
+    });
+    
+    camera= document.getElementById("camera");
+    Webcam.attatch('#camera');
+    
+    function take_snapshot(){
+    Webcam.snap(function(data_uri){  
+    document.getElementById("result").innerHTML = '<img id= "captured_image" src= "'+ data_uri +'"/>';
+    
+    });
+    }
+    
+    console.log("ml5 version", ml5.version);
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/swCMDa9SF/model.json", modelLoaded); 
+    
